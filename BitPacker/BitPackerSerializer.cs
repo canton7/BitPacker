483f6cfe98f098401bb78137b342aef59e51358a
Antony Male <antony.male@gmail.com>
2014-06-08 20:50:59 +0100

    Add type checks on nongeneric serializer

--- BitPacker/BitPackerSerializer.cs
+++ BitPacker/BitPackerSerializer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
@@ -10,15 +11,19 @@ using System.Threading.Tasks;
 
 namespace BitPacker
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public abstract class BitPackerSerializerBase
     {
-        protected Action<BinaryWriter, object> serializer;
+        protected internal Action<BinaryWriter, object> serializer;
+        protected internal Type subjectType;
 
         public bool HasFixedSize { get; private set; }
         public int MinSize { get; private set; }
 
-        public BitPackerSerializerBase(Type subjectType)
+        internal BitPackerSerializerBase(Type subjectType)
         {
+            this.subjectType = subjectType;
+
             var writer = Expression.Parameter(typeof(BinaryWriter), "writer");
             var subject = Expression.Parameter(typeof(object), "subject");
 
@@ -35,12 +40,12 @@ namespace BitPacker
             this.serializer = Expression.Lambda<Action<BinaryWriter, object>>(block, writer, subject).Compile();
         }
 
-        protected void SerializeInternal(BinaryWriter writer, object subject)
+        protected internal void SerializeInternal(BinaryWriter writer, object subject)
         {
             this.serializer(writer, subject);
         }
 
-        protected byte[] SerializeInternal(object subject)
+        protected internal byte[] SerializeInternal(object subject)
         {
             using (var ms = new MemoryStream())
             using (var writer = new BinaryWriter(ms))
@@ -57,13 +62,21 @@ namespace BitPacker
             : base(subjectType)
         { }
 
+        private void CheckType(object subject)
+        {
+            if (!this.subjectType.IsAssignableFrom(subject.GetType()))
+                throw new Exception(String.Format("Serializer for type {0} call with subject of type {1}", this.subjectType, subject.GetType()));
+        }
+
         public void Serialize(BinaryWriter writer, object subject)
         {
+            this.CheckType(subject);
             this.SerializeInternal(writer, subject);
         }
 
         public byte[] Serialize(object subject)
         {
+            this.CheckType(subject);
             return this.SerializeInternal(subject);
         }
     }
