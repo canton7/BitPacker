c38b1ba141a37714f273a15441e81f4463f2018b
Antony Male <antony.male@gmail.com>
2014-06-12 12:20:43 +0100

    Actually add ISerialize

---
+++ BitPacker/ISerialize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitPacker
+{
+    public interface ISerialize
+    {
+        void Serialize(BinaryWriter writer);
+    }
+}
